8446b33e5c168b755e7c2335650841bd0cdc84a4
Noldona <noldona@gmail.com>
2025-01-22 16:21:26 -0500

    Removed files that should be ignored

--- build/niryo_one_msgs/ament_cmake_python/niryo_one_msgs/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='niryo_one_msgs',
-    version='0.0.0',
-    packages=find_packages(
-        include=('niryo_one_msgs', 'niryo_one_msgs.*')),
-)
