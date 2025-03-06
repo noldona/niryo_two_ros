ff57edb75af9559e35057b3e6279cbb188482be5
Eric <ericlashley@rocketmail.com>
2025-03-06 01:01:05 -0500

    commander

--- src/niryo_one_commander/setup.py
+++ src/niryo_one_commander/setup.py
@@ -6,6 +6,7 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    package_dir={'': 'src'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -15,11 +16,12 @@ setup(
     zip_safe=True,
     maintainer='ericlashley',
     maintainer_email='ericlashley@rocketmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Interface between user and Moveit! to easily control Niryo One (arm + tools). Validates params, checks if previous trajectory has not finished, ...',
+    license='GPL-3.0-only',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'niryo_one_commander_node = niryo_one_commander:main',
         ],
     },
 )
