e110e967ca3ccdb41f8a863bb8ae47f79717b97d
Eric <ericlashley@rocketmail.com>
2025-05-01 22:45:39 -0400

    Moveit

--- src/niryo_one_hardware/bringup/launch/niryo_one_controller.launch.py
+++ src/niryo_one_hardware/bringup/launch/niryo_one_controller.launch.py
@@ -83,7 +83,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["niryo_one_controller", "--param-file", robot_controllers],
+        arguments=["niryo_one_controller"],
         # prefix=['gdb -ex run --args'],
     )
 
